63f5c50373051647758c9d5fdc908d039b5a0d41
Kim Chanhwi <kim.ch@hybecorp.com>
2024-06-28 14:00:30 +0900

    I18nConfig에서 MessageSourceAccessor의 로케일 설정을 기본 로케일로 변경

--- src/main/java/boozilla/houston/config/I18nConfig.java
+++ src/main/java/boozilla/houston/config/I18nConfig.java
@@ -22,9 +22,8 @@ public class I18nConfig {
     }
 
     @Bean
-    public MessageSourceAccessor messageSourceAccessor(@Value("${spring.messages.locale}") final String language)
+    public MessageSourceAccessor messageSourceAccessor()
     {
-        final var locale = Locale.of(language);
-        return new MessageSourceAccessor(messageSource(), locale);
+        return new MessageSourceAccessor(messageSource(), Locale.getDefault());
     }
 }
