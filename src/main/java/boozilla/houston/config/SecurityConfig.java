1f4f8b3d7dad6d7d5b037634aadfadc21eae0d56
boozilla <io.async@gmail.com>
2025-03-23 21:04:19 +0900

    `AWS 관련 설정 리팩토링 및 불필요 코드 제거`
    AwsProperties를 제거하고 AwsCredentialsCondition을 추가해 AWS 자격 증명 조건을 재구성했습니다. 이를 통해 보안 설정을 간소화하고 조건 기반 빈 생성 방식으로 변경했습니다. 기존 build.gradle에서 사용되지 않는 gRPC 관련 설정도 삭제하여 불필요한 의존성을 줄였습니다.

--- src/main/java/boozilla/houston/config/SecurityConfig.java
+++ src/main/java/boozilla/houston/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package boozilla.houston.config;
 
-import boozilla.houston.properties.AwsProperties;
+import boozilla.houston.common.AwsCredentialsCondition;
 import boozilla.houston.properties.KmsProperties;
 import boozilla.houston.security.EcdsaKeyProvider;
 import boozilla.houston.security.KmsAlgorithm;
@@ -10,52 +10,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.kms.KmsAsyncClient;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerAsyncClient;
-
-import java.util.Objects;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecretsManagerAsyncClient secretsManagerAsyncClient(final AwsProperties awsProperties)
+    @Conditional(AwsCredentialsCondition.class)
+    public SecretsManagerAsyncClient secretsManagerAsyncClient()
     {
-        final var builder = SecretsManagerAsyncClient.builder();
-
-        if(Objects.nonNull(awsProperties.endpoint()))
-            builder.endpointOverride(awsProperties.endpoint());
-
-        if(Objects.nonNull(awsProperties.accessKey()) && Objects.nonNull(awsProperties.secretKey()))
-            builder.credentialsProvider(StaticCredentialsProvider.create(
-                    AwsBasicCredentials.create(awsProperties.accessKey(), awsProperties.secretKey()))
-            );
-
-        if(Objects.nonNull(awsProperties.region()))
-            builder.region(awsProperties.region());
-
-        return builder.build();
+        return SecretsManagerAsyncClient.builder()
+                .build();
     }
 
     @Bean
-    public KmsAsyncClient kmsAsyncClient(final AwsProperties awsProperties)
+    @Conditional(AwsCredentialsCondition.class)
+    public KmsAsyncClient kmsAsyncClient()
     {
-        final var builder = KmsAsyncClient.builder();
-
-        if(Objects.nonNull(awsProperties.endpoint()))
-            builder.endpointOverride(awsProperties.endpoint());
-
-        if(Objects.nonNull(awsProperties.accessKey()) && Objects.nonNull(awsProperties.secretKey()))
-            builder.credentialsProvider(StaticCredentialsProvider.create(
-                    AwsBasicCredentials.create(awsProperties.accessKey(), awsProperties.secretKey()))
-            );
-
-        if(Objects.nonNull(awsProperties.region()))
-            builder.region(awsProperties.region());
-
-        return builder.build();
+        return KmsAsyncClient.builder()
+                .build();
     }
 
     @Bean
