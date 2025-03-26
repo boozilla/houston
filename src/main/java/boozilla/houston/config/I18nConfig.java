7eebc9d01e976aa2e8f8a9f7a7f5ef4288f0fb72
boozilla <io.async@gmail.com>
2025-03-26 22:48:24 +0900

    I18n 설정에 로케일 지원 추가
    애플리케이션의 로케일을 설정 파일에서 가져오도록 수정했습니다. 로케일 값을 기반으로 MessageSource와 MessageSourceAccessor를 초기화하도록 변경했습니다. 로케일 기본값은 en-US로 설정되며, 사용자는 설정을 통해 변경 가능합니다.

--- src/main/java/boozilla/houston/config/I18nConfig.java
+++ src/main/java/boozilla/houston/config/I18nConfig.java
@@ -1,27 +1,39 @@
 package boozilla.houston.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
+import java.util.Locale;
+
 @Configuration
 public class I18nConfig {
     @Bean
-    public ReloadableResourceBundleMessageSource messageSource()
+    public Locale getLocale(@Value("${app.locale:en-US}") final String localeCode)
+    {
+        return Locale.forLanguageTag(localeCode.replace('_', '-'));
+    }
+
+    @Bean
+    public MessageSource messageSource(final Locale locale)
     {
         final var messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:/messages");
+        messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setDefaultLocale(locale);
 
         return messageSource;
     }
 
     @Bean
-    public MessageSourceAccessor messageSourceAccessor(final MessageSource messageSource)
+    public MessageSourceAccessor messageSourceAccessor(final MessageSource messageSource,
+                                                       final Locale locale)
     {
-        return new MessageSourceAccessor(messageSource);
+        return new MessageSourceAccessor(messageSource, locale);
     }
 }
