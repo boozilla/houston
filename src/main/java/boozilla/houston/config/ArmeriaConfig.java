9320b72a1f0777f885d7d69b6989b29f45ffad02
Kim Chanhwi <kim.ch@hybecorp.com>
2025-03-27 11:36:53 +0900

    TLS 설정 추가
    Armeria 서버에 TLS 지원을 위한 설정을 추가했습니다. TLS 인증서 및 키 파일 경로를 환경 변수로 받아 이를 Armeria 서버 빌더에 적용하도록 구성했습니다. 예제 환경 변수 파일에도 관련 설정 항목을 추가했습니다.

--- src/main/java/boozilla/houston/config/ArmeriaConfig.java
+++ src/main/java/boozilla/houston/config/ArmeriaConfig.java
@@ -1,6 +1,7 @@
 package boozilla.houston.config;
 
 import com.linecorp.armeria.common.HttpHeaderNames;
+import com.linecorp.armeria.common.TlsKeyPair;
 import com.linecorp.armeria.server.ClientAddressSource;
 import com.linecorp.armeria.server.docs.DocService;
 import com.linecorp.armeria.server.docs.DocServiceBuilder;
@@ -11,10 +12,15 @@ import com.linecorp.armeria.spring.ArmeriaBeanPostProcessor;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.netty.channel.ChannelOption;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.util.Objects;
 
 @Configuration
 public class ArmeriaConfig {
@@ -29,6 +35,18 @@ public class ArmeriaConfig {
                 .channelOption(ChannelOption.SO_REUSEADDR, true)
                 .clientAddressSources(ClientAddressSource.ofHeader(HttpHeaderNames.X_FORWARDED_FOR))
                 .meterRegistry(meterRegistry);
+    }
+
+    @Bean
+    public ArmeriaServerConfigurator tls(@Nullable @Value("${armeria.tls.private-key-file}") final String privateKeyFile,
+                                         @Nullable @Value("${armeria.tls.cert-chain-file}") final String certChainFile)
+    {
+        return serverBuilder -> {
+            if(Objects.nonNull(privateKeyFile) && Objects.nonNull(certChainFile))
+            {
+                serverBuilder.tls(TlsKeyPair.of(new File(privateKeyFile), new File(certChainFile)));
+            }
+        };
     }
 
     @Bean
